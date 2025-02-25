8401b0a13da579db29403f8341a9b709ffa54b63
Abdu <abduetmtl@gmail.com>
2025-02-25 21:13:19 +0530

    login screen with image background

--- CoreModule/UI/Admin/encryptz.Admin/src/app/app.routes.ts
+++ CoreModule/UI/Admin/encryptz.Admin/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent) },
-  { path: 'auth', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
+  { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent) },  
   { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent) },
 ];
--- CoreModule/UI/Admin/encryptz.Admin/src/app/auth/login/login.component.ts
+++ CoreModule/UI/Admin/encryptz.Admin/src/app/auth/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin() {
+    if (this.email === 'admin@example.com' && this.password === 'password') {
+      alert('Login Successful');
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
 }
